36f5c2820faaad23c8530584dba382ffb966bc6b
Reggie Gian Escobar <me@reggieescobar.com>
2018-03-12 22:46:59 +0800

    Finished Section A - Luhn Validator Method and parts of Credt Card class

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,11 +1,16 @@
+# Validates credit card number using Luhn Algorithm
 module LuhnValidator
-  # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = nums_a.reverse_each.each_with_index.map do |n, k|
+      next n unless k.odd?
+      n *= 2
+      n = n.to_s.chars.map(&:to_i).reduce(:+) if n > 9
+      n
+    end
+    nums_a.reduce(:+).modulo(10).zero?
   end
 end
