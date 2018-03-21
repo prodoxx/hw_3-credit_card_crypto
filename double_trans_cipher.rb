a9d15e64cca7748405a12c441d3377217c6a286d
Reggie Gian Escobar <me@reggieescobar.com>
2018-03-21 15:44:05 +0800

    Finished substitution_cipher and encryption of double_trans_cipher

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,3 +1,5 @@
+require 'matrix'
+
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
     # TODO: FILL THIS IN!
@@ -7,9 +9,26 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+    document = document.to_s
+    row_col_size = Math.sqrt(document.size).ceil
+    matrix = document.chars.to_a.each_slice(row_col_size).to_a
+    matrix = matrix.map { |s| s.shuffle(random: Random.new(key)) }
+    matrix.shuffle(random: Random.new(key)).join
   end
 
   def self.decrypt(ciphertext, key)
-    # TODO: FILL THIS IN!
+    row_col_size = Math.sqrt(ciphertext.size).ceil
+    original_matrix = ciphertext.to_s.chars.to_a.each_slice(row_col_size).to_a
+    new_matrix = []
+    
+    row_col_size.times do |n1|
+      new_matrix[n1] = []
+      row_col_size.times do |n2|
+        new_matrix[n1][n2] = original_matrix[n2][n1] if !original_matrix[n2][n1].nil?
+      end
+    end
+
+    print new_matrix
+
   end
 end
