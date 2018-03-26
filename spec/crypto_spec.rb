9e62dcf5e240627a7379466b7f52ecff131b8fa8
Reggie Gian Escobar <me@reggieescobar.com>
2018-03-26 12:03:55 +0800

    fixed minor global variable bug with crypto_spec tests

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -6,30 +6,36 @@ require 'minitest/autorun'
 def test_encryption(encryption, document, key, should_encrypt = true)
   should = should_encrypt ? 'should encrypt card information' : 'should decrypt text'
   it should do
-    ed = should_encrypt ? encryption.encrypt(document,key) : encryption.decrypt(document, key)
-    ed.wont_equal @cc.to_s
-    ed.wont_be_nil
+    if should_encrypt
+      ed = encryption.encrypt(document, key)
+      ed.wont_be_nil
+      ed.wont_equal document.to_s
+    else
+      en = encryption.encrypt(document, key)
+      ed = encryption.decrypt(en, key)
+      ed.wont_be_nil
+      ed.must_equal document.to_s
+    end
   end
 end
 
+cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
+key = 3
+
 describe 'Test card info encryption' do
-  before do
-    @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
-    @key = 3
-  end
-  
+
   describe 'Using Caesar cipher' do
-    test_encryption(SubstitutionCipher::Caesar, @cc, @key)
-    test_encryption(SubstitutionCipher::Caesar, @cc, @key, false)
+    test_encryption(SubstitutionCipher::Caesar, cc, key)
+    test_encryption(SubstitutionCipher::Caesar, cc, key, false)
   end
 
   describe 'Using Permutation cipher' do
-    test_encryption(SubstitutionCipher::Permutation, @cc, @key)
-    test_encryption(SubstitutionCipher::Permutation, @cc, @key, false)
+    test_encryption(SubstitutionCipher::Permutation, cc, key)
+    test_encryption(SubstitutionCipher::Permutation, cc, key, false)
   end
 
   describe 'Using Double Transposition cipher' do
-    test_encryption(DoubleTranspositionCipher, @cc, @key)
-    test_encryption(DoubleTranspositionCipher, @cc, @key, false)
+    test_encryption(DoubleTranspositionCipher, cc, key)
+    test_encryption(DoubleTranspositionCipher, cc, key, false)
   end
 end
